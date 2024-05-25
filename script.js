a9a24bf062f74fe20d554ae01c225698a64c4d76
stinejulius <stine.insecure@protonmail.com>
2024-05-25 19:36:52 +0200

    Optimized all svg files

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ document.addEventListener("click", function (event) {
 // Function that runs with every click on the document
 function onDocumentClick(event) {
     // If event.target is either the menuContent or the menu, then we don't do anything
+    // Event.target is the Element that has been clicked on
     if (menuContent.contains(event.target) || menu.contains(event.target)) {
         return;
     }
@@ -41,11 +42,10 @@ function onDocumentClick(event) {
 // Find projects, this will contain x amount of project Elements (project-box from HTML)
 let projects = document.getElementsByClassName("project-box");
 
-
 // Makes it go through each project in projects
 for (let project of projects) {
 
-    // Gets the arrow element in the project is is looking in (snapshot)
+    // Gets the arrow element in the project it is looking in (snapshot)
     let projectArrows = project.getElementsByClassName("project-box-arrow-down");
 
     // Since theres only 1 arrow we know, we just have to take the first - it counts from 0 and up
@@ -72,11 +72,13 @@ let qualifications = document.getElementsByClassName("qualification");
 // For quali-top
 for (let qualification of qualifications) {
 
+    // Gets the arrow element in the qualification it is looking in
     let qualiTopArrows = qualification.getElementsByClassName("quali-top");
 
     // Because there only is 1 arrow its 0 - it always counts from 0 and up
     let qualiTopArrow = qualiTopArrows[0];
 
+    // Adds EventListener, that listens to a "click" event on the arrow element
     qualiTopArrow.addEventListener("click", function (event) {
         onQualiClick(qualification);
     });
@@ -86,18 +88,21 @@ for (let qualification of qualifications) {
 // For quali-bottom
 for (let qualification of qualifications) {
 
+    // Gets the arrow element in the qualification it is looking in
     let qualiBottomArrows = qualification.getElementsByClassName("quali-bottom");
 
     // Because there only is 1 arrow its 0 - it always counts from 0 and up
     let qualiBottomArrow = qualiBottomArrows[0];
 
+    // Adds EventListener, that listens to a "click" event on the arrow element
     qualiBottomArrow.addEventListener("click", function (event) {
         onQualiClick(qualification);
     });
 }
 
-// Turns on the class .active in CSS
+// Function that runs with every click on the specified qualification (it waits for the "click" event)
 function onQualiClick(qualification) {
+    // Turns on the class .active in CSS
     qualification.classList.toggle("active");
 }
 
@@ -110,17 +115,21 @@ let hobbies = document.getElementsByClassName("about-hobby-box")
 // For about-box-arrow-down
 for (let hobby of hobbies) {
 
+    // Gets the arrow element in the hobby it is looking in
     let hobbyArrows = hobby.getElementsByClassName("about-box-arrow-down");
 
     // Because there only is 1 arrow its 0 - it always counts from 0 and up
     let hobbyArrow = hobbyArrows[0];
 
+    // Adds EventListener, that listens to a "click" event on the arrow element
     hobbyArrow.addEventListener("click", function (event) {
         onHobbyClick(hobby);
     });
 }
 
-// Turns on the class .active in CSS
+
+// Function that runs with every click on the specified hobby (it waits for the "click" event)
 function onHobbyClick(hobby) {
+    // Turns on the class .active in CSS
     hobby.classList.toggle("active");
 }
